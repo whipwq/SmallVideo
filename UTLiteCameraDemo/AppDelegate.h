362e2dce8d03c86d043c83e4e1d8975d2c47dd83
温强 <364443982@qq.com>
2018-04-18 11:24:07 +0800

    新建

---
+++ UTLiteCameraDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UTLiteCameraDemo
+//
+//  Created by yons on 17/2/20.
+//  Copyright © 2017年 WQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
